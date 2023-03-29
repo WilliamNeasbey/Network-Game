41500a81c01ffd0457e73c0437a9fcdf6c14e7f5
MicroSillyWilly <1025946@student.sae.edu.au>
2023-03-30 00:05:16 +1100

    mega L

---
+++ Assets/Scripts/Network2DCharacter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Unity.Netcode;
+
+
+public class Network2DCharacter : MonoBehaviour
+{
+  
+}
